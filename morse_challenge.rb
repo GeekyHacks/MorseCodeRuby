99d2b8f286266457a1870bf0535c5de83700f110
Abdullah <geekyhacks22@gmail.com>
2023-10-13 18:27:18 +0300

    Added the required changes

--- morse_challenge.rb
+++ morse_challenge.rb
@@ -1,9 +1,22 @@
-def decode(string)
+def decode_char(char)
   morse_code = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
                  '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M',
                  '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S',
                  '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
-  string.split.map { |letter| morse_code[letter] }.join(' ')
+  morse_code[char] || '[UNKNOWN]'
 end
 
-puts decode('-- -.--   -. .- -- .')
+def decode_word(word)
+  decoded_letters = word.split.map { |letter| decode_char(letter) }
+  decoded_letters.join('')
+end
+
+def decode_sentence(sentence)
+  words = sentence.split('   ')
+  decoded_words = words.map { |word| decode_word(word) }
+  decoded_words.join(' ')
+end
+
+puts decode_char('--')
+puts decode_word('-- -.--   -. .- -- .')
+puts decode_sentence('-- -.--     -. .- -- .')
